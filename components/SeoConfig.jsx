552397ecec23aa466fd7aa3a31a5a3e9b5453c19
Icaro Albar <icaro.albar@live.com>
2022-10-21 10:06:30 -0300

    SEO configurado

---
+++ components/SeoConfig.jsx
@@ -0,0 +1,7 @@
+const title = 'Plano de Saúde Unimed Rio'
+const description = 'Todos os planos de saúde Unimed e simulador de planos de saúde online'
+
+const SEO = {
+    title,
+    description
+}
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,26 @@
 import '../styles/globals.css'
+import { DefaultSeo } from 'next-seo'
+import SEO from '../components/SeoConfig'
+import TagManager from 'react-gtm-module'
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  useEffect(() => {
+    const tagManagerArgs = {
+      gtmId: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID
+    }
+
+    TagManager.initialize(tagManagerArgs)
+
+  }, [])
+
+  return (
+    <>
+      <DefaultSeo {...SEO} />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
