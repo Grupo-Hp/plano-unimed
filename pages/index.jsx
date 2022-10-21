632dea3a5420326db728b48076e512006703eef9
Icaro Albar <icaro.albar@live.com>
2022-10-21 11:55:06 -0300

    Cards atualizados

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,6 +2,7 @@ import Faixa1 from '../components/Faixa1'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 import Info1 from '../components/Info1'
+import Cards from '../components/Cards'
 import Info2 from '../components/Info2'
 import Info3 from '../components/Info3'
 import Nav from '../components/Nav'
@@ -13,6 +14,7 @@ export default function Home() {
       <Header />
       <Faixa1 />
       <Info1 />
+      <Cards />
       <Info2 />
       <Info3 />
       <Footer />
