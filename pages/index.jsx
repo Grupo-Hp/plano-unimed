a4c8ed273a26dcee29e403c95a976e088c664668
Icaro Albar <icaro.albar@live.com>
2022-10-21 09:46:33 -0300

    Paleta de cores configurada

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,6 +2,17 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <h1>Hello World!</h1>
+    <>
+      <h1 className='text-primary-10'>Hello World!</h1>
+      <h1 className='text-primary-20'>Hello World!</h1>
+      <h1 className='text-primary-30'>Hello World!</h1>
+      <h1 className='text-secondary-10'>Hello World!</h1>
+      <h1 className='text-secondary-20'>Hello World!</h1>
+      <h1 className='text-secondary-30'>Hello World!</h1>
+      <h1 className='text-secondary-40'>Hello World!</h1>
+      <h1 className='text-secondary-50'>Hello World!</h1>
+      <h1 className='text-secondary-60'>Hello World!</h1>
+      <h1 className='text-secondary-70'>Hello World!</h1>
+    </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,24 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          10: '#00995D',
+          20: '#B1D34B',
+          30: '#FFF0C7',
+        },
+        secondary: {
+          10: '#ED1651',
+          20: '#A3238E',
+          30: '#F47920',
+          40: '#00401A',
+          50: '#411564',
+          60: '#0A5F55',
+          70: '#682D00',
+        },
+      }
+    },
   },
   plugins: [],
 }
