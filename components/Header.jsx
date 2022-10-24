e543d0336b9c812fc78ef07f801616a1d36d2fba
Icaro Albar <icaro.albar@live.com>
2022-10-24 09:24:01 -0300

    Reparo do Header

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-scroll";
 
 export default function Header() {
     return (
-        <div className="headerImage1 sm:h-[80vh] flex items-center">
+        <div className="headerImage1 flex items-center">
             <div className="grid grid-cols-2 w-10/12 mx-auto">
                 <div className="col-span-2 xl:col-span-1 rgba-10 p-10 rounded-xl text-white my-16 br-primary-20">
                     <h3 className="text-6xl font-light mb-5">Reduza o valor do seu plano de saúde</h3>
